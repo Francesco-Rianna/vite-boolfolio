6cb7e678fa1c6f5f509efcc4d010c34fa4da86f7
Francesco-Rianna <maradonino98@hotmail.it>
2024-06-12 20:39:56 +0200

    add component singleproject

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name:'SinglePost'
+}
+</script>
+<template>
+    <div class="container">
+        <div  class="" style="width: 24rem;">
+            <div class="card-body">
+                <h5 class="card-title"></h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary"></h6>
+                <p class="card-text"></p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+</style>
