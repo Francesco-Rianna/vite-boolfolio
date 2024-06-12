2e0f86b66d4ee683b4605bab05b8adf9f8796fee
Francesco-Rianna <maradonino98@hotmail.it>
2024-06-12 20:33:30 +0200

    populated card with data

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -25,12 +25,12 @@ export default {
 </script>
 
 <template>
-    <div class="container">
-        <div v-for="project in projects" class="card" style="width: 18rem;">
+    <div class="container d-flex flex-wrap gap-2">
+        <div v-for="project in projects" class="card mb-3" style="width: 14rem;">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <h5 class="card-title">{{project.name}}</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">{{project.client_name}}</h6>
+                <p class="card-text">{{project.summary}}.</p>
                 <a href="#" class="card-link">Card link</a>
                 <a href="#" class="card-link">Another link</a>
             </div>
